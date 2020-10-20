1a51a584b31d6fa53049891e45f10d90faf9a587
Maxwell99933 <72512376+Maxwell99933@users.noreply.github.com>
2020-10-20 14:06:41 +0300

    Update app_dev.php
    Отключил Кеш

--- shared/homeless/web/app_dev.php
+++ shared/homeless/web/app_dev.php
@@ -28,7 +28,7 @@ $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
+#$kernel->loadClassCache();// Отключил Кеш
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
