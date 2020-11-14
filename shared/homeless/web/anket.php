874e148f0d02e5bee7eb3ef4a7792bf10b4462a9
Maxwell99933 <72512376+Maxwell99933@users.noreply.github.com>
2020-11-14 11:18:38 +0300

    Update anket.php

--- shared/homeless/web/anket.php
+++ shared/homeless/web/anket.php
@@ -1,4 +1,17 @@
-<?php
+<!doctype html>
+<html lang="ru">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Anketa</title>
+    
+    <?php
 $Name = $_POST['Name'];
 $Suname = $_POST['Suname'];
 $MiddleName = $_POST['MiddleName'];
@@ -19,21 +32,7 @@ $Occupation = $_POST['Occupation'];
 $OccupationOther = $_POST['OccupationOther'];
 $Contact = $_POST['Contact'];
 ?>
-
-
-
-<!doctype html>
-<html lang="ru">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <title>Anketa</title>
+    
 </head>
 
 <body>
